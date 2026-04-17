2496a3785d405d6c6f54073cfb0c16229a4efaac
Dorian Abdullahi <dabdulla@c2r1p9.42vienna.com>
2026-04-17 14:33:12 +0200

    shorter ver

--- exam_03/gnl/get_next_line.c
+++ exam_03/gnl/get_next_line.c
@@ -3,43 +3,31 @@
 char	*get_next_line(int fd)
 {
 	static char	*buffer = NULL;
-	static int cursor = 0;
-	static int b_len = -1;
-	int r;
-	int i;
-	char *line;
-	int end_line;
+	static int	cursor = 0;
+	static int	b_len = -1;
+	int			r = 1, i = 0, end_line = 0;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || cursor == b_len)
 		return (free(buffer), NULL);
-	i = 0;
-	end_line = 0;
 	if (!buffer)
 		buffer = malloc(1024);
-	r = 1;
 	while (r > 0)
 	{
 		r = read(fd, buffer + end_line, BUFFER_SIZE);
 		if (r == 0)
-			break;
+			break ;
 		end_line += r;
 		buffer[end_line] = '\0';
 		b_len = end_line;
-		
 	}
 	line = malloc(1024);
 	while (cursor < b_len)
 	{
 		line[i] = buffer[cursor];
-		if (buffer[cursor] == '\n')
-		{
-			cursor++;
-			i++;
-			line[i] = '\0';
-			return (line);
-		}
 		i++;
-		cursor++;
+		if (buffer[cursor++] == '\n')
+			break;
 	}
 	line[i] = '\0';
 	if (!line || *line == 0)
@@ -52,10 +40,12 @@ char	*get_next_line(int fd)
 
 int	main(int ac, char *av[])
 {
+	int		fd;
+	char	*line;
+
 	if (ac != 2)
-		return 1;
-	int fd = open(av[1], O_RDONLY);
-	char *line;
+		return (1);
+	fd = open(av[1], O_RDONLY);
 	while ((line = get_next_line(fd)))
 	{
 		printf("%s", line);
